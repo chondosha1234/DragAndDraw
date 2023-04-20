7c80693b95f751d7a7d98358baaff25e6fa8a6a8
chondosha1234 <jmiller61193@gmail.com>
2023-04-20 16:07:31 -0400

    initial commit

---
+++ app/src/main/java/com/chondosha/draganddraw/BoxDrawingView.kt
@@ -0,0 +1,13 @@
+package com.chondosha.draganddraw
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class BoxDrawingView(
+    context: Context,
+    attrs: AttributeSet? = null,
+) : View(context, attrs) {
+
+
+}
