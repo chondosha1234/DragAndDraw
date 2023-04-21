5df4f9c0129a858002078166c11844b09d4845c9
chondosha1234 <jmiller61193@gmail.com>
2023-04-20 22:22:35 -0400

    save state between orientation switches. made Box class parcelable

--- app/src/main/java/com/chondosha/draganddraw/Box.kt
+++ app/src/main/java/com/chondosha/draganddraw/Box.kt
@@ -1,12 +1,36 @@
 package com.chondosha.draganddraw
 
 import android.graphics.PointF
+import android.os.Parcel
+import android.os.Parcelable
 import kotlin.math.max
 import kotlin.math.min
 
-data class Box(val start: PointF) {
+data class Box(val start: PointF) : Parcelable {
 
     var end: PointF = start
+
+    constructor(parcel: Parcel) : this(parcel.readParcelable<PointF>(PointF::class.java.classLoader)!!) {
+        end = parcel.readParcelable(PointF::class.java.classLoader)!!
+    }
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeParcelable(start, flags)
+        parcel.writeParcelable(end, flags)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Box> {
+        override fun createFromParcel(parcel: Parcel): Box {
+            return Box(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Box?> {
+            return arrayOfNulls(size)
+        }
+    }
 
     val left: Float
         get() = min(start.x, end.x)
--- app/src/main/java/com/chondosha/draganddraw/BoxDrawingView.kt
+++ app/src/main/java/com/chondosha/draganddraw/BoxDrawingView.kt
@@ -4,10 +4,14 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
 import android.graphics.PointF
+import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
 import android.util.AttributeSet
 import android.util.Log
 import android.view.MotionEvent
 import android.view.View
+import android.view.View.BaseSavedState
 
 private const val TAG = "BoxDrawingView"
 
@@ -19,7 +23,7 @@ class BoxDrawingView(
     private var currentBox: Box? = null
     private val boxes = mutableListOf<Box>()
     private val boxPaint = Paint().apply {
-        color = 0x22ff0000.toInt()
+        color = 0x22ff0000
     }
     private val backgroundPaint = Paint().apply {
         color = 0xfff8efe0.toInt()
@@ -51,7 +55,7 @@ class BoxDrawingView(
                 currentBox = null
             }
         }
-        Log.d(TAG, "$action at x=${current.x}, y=${current.y} ")
+        //Log.d(TAG, "$action at x=${current.x}, y=${current.y} ")
 
         return true
     }
@@ -69,6 +73,54 @@ class BoxDrawingView(
         currentBox?.let {
             it.end = current
             invalidate()
+        }
+    }
+
+    override fun onSaveInstanceState(): Parcelable? {
+        val superState = super.onSaveInstanceState()
+        val state = Bundle()
+        state.putParcelableArrayList(STATE_BOXES, ArrayList(boxes))
+        return SavedState(superState, state)
+    }
+
+    override fun onRestoreInstanceState(state: Parcelable?) {
+        if (state is SavedState) {
+            super.onRestoreInstanceState(state)
+            boxes.clear()
+            boxes.addAll(state.state.getParcelableArrayList(STATE_BOXES) ?: emptyList())
+        } else {
+            super.onRestoreInstanceState(state)
+        }
+    }
+
+    companion object {
+        private const val STATE_BOXES = "boxes"
+    }
+}
+
+private class SavedState : BaseSavedState {
+    val state: Bundle
+
+    constructor(superState: Parcelable?, state: Bundle) : super(superState) {
+        this.state = state
+    }
+
+    constructor(source: Parcel) : super(source) {
+        state = source.readBundle(javaClass.classLoader) ?: Bundle()
+    }
+
+    override fun writeToParcel(out: Parcel, flags: Int) {
+        super.writeToParcel(out, flags)
+        out.writeBundle(state)
+    }
+
+    companion object CREATOR : Parcelable.Creator<SavedState> {
+        override fun createFromParcel(source: Parcel): SavedState {
+            return SavedState(source)
+        }
+
+        override fun newArray(size: Int): Array<SavedState?> {
+            return arrayOfNulls(size)
         }
     }
 }
